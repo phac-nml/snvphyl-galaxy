711aabff0b0147d1fa19097106db56b41b185b21
Aaron Petkau <aaron.petkau@phac-aspc.gc.ca>
2015-02-19 09:25:17 -0600

    Removed unneeded script and moved build toolshed script

--- scripts/build_for_toolshed.sh
+++ scripts/build_for_toolshed.sh
@@ -1,11 +1,10 @@
 #!/bin/bash
 
 ROOT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
-BUILD_DIR="$ROOT_DIR/build"
-TOOLS_DIR="$ROOT_DIR/tools"
-WORKFLOWS_DIR="$ROOT_DIR/workflows"
+BUILD_DIR="$ROOT_DIR/../build"
+TOOLS_DIR="$ROOT_DIR/../tools"
 
-echo "Builds tool and workflow tarballs under build/."
+echo "Builds tool and workflow tarballs under $BUILD_DIR"
 echo "These can then be uploaded to a Galaxy Tool Shed."
 
 if [ ! -e $BUILD_DIR ]
@@ -19,8 +18,6 @@ do
 	name=`basename $i`
 	tar -C $TOOLS_DIR/$name -czf $BUILD_DIR/$name.tar.gz .
 done
-
-tar -C $WORKFLOWS_DIR/core_phylogenomics_pipeline_workflow -czf $BUILD_DIR/core_phylogenomics_pipeline_workflow.tar.gz .
 
 echo "Successfully built tarballs"
 ls -l $BUILD_DIR/*.tar.gz
