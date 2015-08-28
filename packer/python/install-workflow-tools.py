d80c5bead9419ff8f7c065c7088dfd6b16f55049
Aaron Petkau <aaron.petkau@phac-aspc.gc.ca>
2015-08-28 15:50:50 -0500

    Updated packer build to properly install tools and workflows

--- packer/python/install-workflow-tools.py
+++ packer/python/install-workflow-tools.py
@@ -3,20 +3,19 @@ import argparse, os, string, json
 import xml.etree.ElementTree as ET
 from bioblend import galaxy
 
-def main(master_api_key, api_url, galaxy_admin_user, galaxy_admin_pass, galaxy_workflow_user, galaxy_workflow_pass, pipeline_xml_dir):
+def main(master_api_key, api_url, galaxy_admin_user, galaxy_admin_pass, workflows_dir):
     gi = galaxy.GalaxyInstance(url=api_url, key=master_api_key)
 
     #adding new user
     admin_key = create_user(gi, galaxy_admin_user.split('@')[0], galaxy_admin_user, galaxy_admin_pass)
-    workflow_key = create_user(gi, galaxy_workflow_user.split('@')[0], galaxy_workflow_user, galaxy_workflow_pass)
 
-    print "Galaxy API key created for user. Please use API key [%s] in /etc/irida/irida.conf." % (workflow_key)
+    print "Galaxy API key [%s] created for user [%s]." % (admin_key,galaxy_admin_user)
 
     print "Signing in as admin user."
     gi = galaxy.GalaxyInstance(url=api_url, key=admin_key)
 
-    for (dir_name, sub_dir, file_names) in os.walk(pipeline_xml_dir):
-        for xml_file in filter(lambda f: f.endswith('.xml'), file_names):
+    for (dir_name, sub_dir, file_names) in os.walk(workflows_dir):
+        for xml_file in filter(lambda f: f == 'workflow_dependencies.xml', file_names):
             xml_path = os.path.join(dir_name, xml_file)
             print "Going to parse " + xml_path
             xml_doc = ET.parse(xml_path)
@@ -38,6 +37,11 @@ def main(master_api_key, api_url, galaxy_admin_user, galaxy_admin_pass, galaxy_w
                     else:
                         print "%s failed to install, check the galaxy logs." % tool_info
                         raise
+
+        for workflow_file in filter(lambda f: f == 'snvphyl-workflow.ga', file_names):
+            workflow_path = os.path.join(dir_name, workflow_file)
+            print "Going to load " + workflow_path
+            gi.workflows.import_workflow_from_local_path(workflow_path)
     return
 
 def create_user(gi, username, email, password):
@@ -49,12 +53,11 @@ def create_user(gi, username, email, password):
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='Install Galaxy tools.')
     
-
     parser.add_argument('--master-api-key', action="store", dest="master_api_key",required=True)
     parser.add_argument('--master-api-url', action="store", dest="api_url",required=True)
     parser.add_argument('--galaxy-admin-user', action="store", dest="galaxy_admin_user", required=True)
     parser.add_argument('--galaxy-admin-pass', action="store", dest="galaxy_admin_pass", required=True)
-    parser.add_argument('--workflow-dir', action="store", dest="pipeline_xml_dir", required=True)
+    parser.add_argument('--workflows-dir', action="store", dest="workflows_dir", required=True)
     args = parser.parse_args()
     dic = vars(args)
 
--- packer/scripts/galaxy.sh
+++ packer/scripts/galaxy.sh
@@ -21,6 +21,9 @@ yum -y install epel-release
 yum -y install mercurial pwgen python zlib-devel ncurses-devel tcsh db4-devel expat-devel java-1.8.0-openjdk-headless python-pip perl-App-cpanminus gnuplot libyaml-devel python-devel cmake mariadb-server mariadb-client
 yum -y groupinstall "Development Tools"
 
+# install perl dependencies (force because bioperl fails spuriously)
+cpanm --force Fatal Clone Parallel::ForkManager http://search.cpan.org/CPAN/authors/id/C/CJ/CJFIELDS/BioPerl-1.6.901.tar.gz Time::Piece XML::Simple Data::Dumper autodie
+
 pip install bioblend
 
 # prepare the directories and check out galaxy
@@ -31,13 +34,19 @@ chown -R $GALAXY_USER $GALAXY_HOME
 
 function config_galaxy () {
 	cd $DEPENDENCIES_INSTALL
+	curl -L -O http://downloads.sourceforge.net/project/mummer/mummer/3.23/MUMmer3.23.tar.gz
+	tar xf MUMmer3.23.tar.gz
+	cd MUMmer3.23
+	make
+
+	cd $DEPENDENCIES_INSTALL
 	curl -L -O http://downloads.sourceforge.net/project/samtools/samtools/0.1.18/samtools-0.1.18.tar.bz2
 	tar xf samtools-0.1.18.tar.bz2
 	cd samtools-0.1.18
 	make
 
 	cat >> $GALAXY_ENV <<EOF
-PATH=$DEPENDENCIES_INSTALL/samtools-0.1.18:$DEPENDENCIES_INSTALL/samtools-0.1.18/bcftools:/bin:/usr/bin
+PATH=$DEPENDENCIES_INSTALL/MUMmer3.23:$DEPENDENCIES_INSTALL/samtools-0.1.18:$DEPENDENCIES_INSTALL/samtools-0.1.18/bcftools:/bin:/usr/bin
 EOF
 
 	cd $GALAXY_HOME
@@ -57,9 +66,9 @@ EOF
         sed -i 's_#host.*_host = 0.0.0.0_' $GALAXY_HOME/galaxy/config/galaxy.ini
 	sed -i 's_#port.*_port = 8080_' $GALAXY_HOME/galaxy/config/galaxy.ini
 	# use 127.0.0.1 instead of localhost; localhost tries to connect over a socket, 127.0.0.1 uses tcp
-	sed -i 's_#database\_connection.*_database\_connection = mysql://irida:irida@127.0.0.1/galaxy\_irida_' $GALAXY_HOME/galaxy/config/galaxy.ini
+	sed -i "s_#database\_connection.*_database\_connection = mysql://$GALAXY_USER:$GALAXY_USER@127.0.0.1/${GALAXY_DATABASE}_" $GALAXY_HOME/galaxy/config/galaxy.ini
 	sed -i 's/#allow_library_path_paste.*/allow_library_path_paste = True/' $GALAXY_HOME/galaxy/config/galaxy.ini
-	sed -i 's/#admin_users.*/admin_users = admin@localhost.localdomain, workflow@localhost.localdomain/' $GALAXY_HOME/galaxy/config/galaxy.ini
+	sed -i 's/#admin_users.*/admin_users = admin@localhost.localdomain/' $GALAXY_HOME/galaxy/config/galaxy.ini
 	sed -i 's_debug = True_debug = False_' $GALAXY_HOME/galaxy/config/galaxy.ini
 	sed -i 's/use_interactive = True/use_interactive = False/' $GALAXY_HOME/galaxy/config/galaxy.ini
 	sed -i 's_^filter-with = gzip_#&_' $GALAXY_HOME/galaxy/config/galaxy.ini
@@ -72,15 +81,14 @@ EOF
 	sed -i "s@#environment_setup_file = None@environment_setup_file = $GALAXY_ENV@" $GALAXY_HOME/galaxy/config/galaxy.ini
 
 	## tool_sheds_conf.xml
-	#sed -i 's@</tool_sheds>@    <tool_shed name="IRIDA tool shed" url="https://irida.corefacility.ca/galaxy-shed/"/>\n</tool_sheds>@' $GALAXY_HOME/galaxy/config/tool_sheds_conf.xml
-	sed -i 's@</tool_sheds>@    <tool_shed name="NML tool shed" url="http://galaxy-shed.corefacility.ca/"/>\n</tool_sheds>@' $GALAXY_HOME/galaxy/config/tool_sheds_conf.xml
+	sed -i 's@</tool_sheds>@    <tool_shed name="IRIDA tool shed" url="https://irida.corefacility.ca/galaxy-shed/"/>\n	<tool_shed name="NML tool shed" url="http://galaxy-toolshed.corefacility.ca/"/>\n</tool_sheds>@' $GALAXY_HOME/galaxy/config/tool_sheds_conf.xml
 }
 
 export -f config_galaxy
 su $GALAXY_USER -c 'config_galaxy'
 
 systemctl start mariadb
-echo "grant all privileges on $GALAXY_DATABASE* to '$GALAXY_USER'@'localhost' identified by '$GALAXY_USER';" | mysql -u root
+echo "grant all privileges on $GALAXY_DATABASE.* to '$GALAXY_USER'@'localhost' identified by '$GALAXY_USER';" | mysql -u root
 echo "create database $GALAXY_DATABASE;" | mysql -u root
 
 # Write out a systemd startup script for galaxy
@@ -113,9 +121,7 @@ mv /tmp/install-workflow-tools.py $DEPENDENCIES_INSTALL
 MASTER_API_KEY=$(grep master_api_key $GALAXY_HOME/galaxy/config/galaxy.ini | awk '{print $3}')
 
 cd $DEPENDENCIES_INSTALL
-#python install-workflow-tools.py --pipeline-xml-dir workflows/ --master-api-key $MASTER_API_KEY --master-api-url http://localhost:8080/api --galaxy-admin-user admin@localhost.localdomain --galaxy-admin-pass adminpassword --galaxy-workflow-user workflow@localhost.localdomain --galaxy-workflow-pass workflowpassword | tee install-workflow-tools.log
-
-#API_KEY=$(grep 'Galaxy API key created for user' install-workflow-tools.log | perl -pe 's/.*\[([^\]]+)\].*/\1/')
+python install-workflow-tools.py --workflows-dir workflows/ --master-api-key $MASTER_API_KEY --master-api-url http://localhost:8080/api --galaxy-admin-user admin@localhost.localdomain --galaxy-admin-pass adminpassword | tee install-workflow-tools.log
 
 systemctl stop galaxy
 systemctl stop mariadb
