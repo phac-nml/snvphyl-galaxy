5f182922d0e6a98f88adeaaf4d6c2ebf20501c78
Aaron Petkau <aaron.petkau@phac-aspc.gc.ca>
2015-09-02 13:13:03 -0500

    Revert "Removed text not displaying correctly"
    This reverts commit 142f5a948aa0536eead1842c28d8107b64822974.

--- packer/scripts/base.sh
+++ packer/scripts/base.sh
@@ -10,9 +10,9 @@ Welcome to the Galaxy/SNVPhyl Virtual Environment.
 
 As you can see, the Virtual Environment does not have a GUI installed. You should interact with Galaxy/SNVPhyl using your web browser.
 
-You can access the Galaxy web interface by navigating to http://localhost:48888/ in your web browser. The default username and password is admin@localhost.localdomain and adminpassword.
+You can access the Galaxy web interface by navigating to http://localhost:48888/ in your web browser. The default username and password is "admin@localhost.localdomain" and "adminpassword".
 
-You can log into this virtual environment by using the username vagrant and the password vagrant. Alternatively, you can SSH into this virtual environment by running ssh -p2222 vagrant@localhost and using the password vagrant.  This account has sudo access.  The Galaxy log files are located at /opt/galaxy/galaxy/paster.log.
+You can log into this virtual environment by using the username "vagrant" and the password "vagrant". Alternatively, you can SSH into this virtual environment by running "ssh -p42222 vagrant@localhost" and using the password "vagrant".  This account has "sudo" access.  The Galaxy log files are located at "/opt/galaxy/galaxy/paster.log".
 
 EOF
 
